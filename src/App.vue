dffde4993f35859fc36514d83331a1e8aa266c0d
Polash Mahmud <polashmahmud@gmail.com>
2023-06-13 23:21:54 +0600

    add login function

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <form v-on:submit.prevent="login">
+    <button type="submit">Login</button>
+  </form>
+</template>
+
+<script setup>
+import axios from "axios";
+
+const login = async () => {
+  await axios.get('/sanctum/csrf-cookie');
+  await axios.post('/login', {
+    email: 'polashmahmud@gmail.com',
+    password: 'password'
+  });
+}
+</script>
