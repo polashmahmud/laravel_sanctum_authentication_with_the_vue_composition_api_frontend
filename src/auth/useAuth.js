80adb4064b73a0d789577b9ea1ac97213f811603
Polash Mahmud <polashmahmud@gmail.com>
2023-06-16 18:31:17 +0600

    fix useAuth

--- src/auth/useAuth.js
+++ src/auth/useAuth.js
@@ -40,6 +40,7 @@ export default function useAuth() {
     return {
         login,
         getUser,
-        getAuthenticated
+        getAuthenticated,
+        attempt
     }
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import './style.css'
 import App from './App.vue'
 import axios from "axios";
 import router from "./router/index.js";
+import useAuth from "./auth/useAuth.js";
+
+const { attempt } = useAuth()
 
 axios.defaults.baseURL = 'http://localhost:8000'
 axios.defaults.withCredentials = true
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+
+attempt().then(() => {
+    app.mount('#app')
+})
+
+
